c056a3a2ed549f0cf92d8e4c6de2d08f03e3d1ea
Lalit Kumar <kumar932687@gmail.com>
2026-07-01 11:50:19 +0530

    updaetd code

---
+++ src/lib/constentData.js
@@ -0,0 +1,37 @@
+import certificateImg from "@/app/api/helperfile/Images/CertificateImg";
+import logo from "@/app/api/helperfile/Images/logo";
+import semkariLogo from "@/app/api/helperfile/Images/semkariLogo";
+
+export const TrsutData={
+    name:"मित्रा हिंदू समाज सेवा संस्थान",
+    cityState:"सुरत-गुजरात",
+    address:"पता:- प्रताप चौक, रबारियों का टांका, बलोतरा – 344022 (राज.)",
+    contact:"कार्यालय नं. – 9166945821 (धनराज सोलंकी)",
+    contactPerson:"धनराज सोलंकी",
+    trustPresident:"धनराज सोलंकी",
+    frameImg:certificateImg,
+    email:"",
+    website:"",
+    regNo:null,
+    logo:logo,
+    RightLogo:null,
+    headerImg:"/Images/headerImg.png",
+    topTitle:[
+        "॥ ॐ जय श्री गणेशाय नमः ॥",
+    ]
+}
+
+export const pdfColors={
+bgColor:'#ffffff',
+borderColor:'#0d5497',
+headingColor:'#eb1e23',
+schemeColor:'#1a0f5e',
+infoLabelColor:'#EA1F25',
+infoValueColor:'#1a0f5e',
+filterLabelColor:'#8B0000',
+filterValueColor:'#1a0f5e',
+summaryColor:'#1a0f5e',
+totalCountColor:'#d4af37',
+programHeaderColor:'#1a0f5e',
+programHeaderBorderColor:'#d4af37',
+}
